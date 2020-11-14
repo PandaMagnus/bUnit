0442aea7ed7ab024201dbc642e97e6b177f700fe
Mike Curn <mjcurn@gmail.com>
2020-11-14 14:03:20 -0800

    Stub out selenium mock example

--- tests/bunit.web.e2e.tests/PageComponentTests.cs
+++ tests/bunit.web.e2e.tests/PageComponentTests.cs
@@ -1,12 +1,26 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
+using Moq;
+using OpenQA.Selenium;
 using Xunit;
 
 namespace bunit.web.e2e.tests
 {
-    public class PageComponentTests
+	public class PageComponentTests
     {
+		[Fact]
+		public void ExampleSeleniumMock()
+		{
+			var mockElement = new Mock<IWebElement>();
+			mockElement.SetupProperty(e => e.Displayed, true);
 
+			var mockDriver = new Mock<IWebDriver>();
+			mockDriver.SetupGet(t => t.Title).Returns("Test Title");
+			mockDriver.Setup(f => f.FindElement(By.CssSelector("someCssSelector")))
+				.Returns(mockElement.Object);
+
+			IWebDriver driverTest = mockDriver.Object;
+			Assert.NotNull(driverTest);
+			Assert.NotNull(driverTest.FindElement(By.CssSelector("someCssSelector")));
+			Assert.Null(driverTest.FindElement(By.CssSelector("someOtherCssSelector")));
+		}
     }
 }
